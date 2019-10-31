7afebd6ee4bf56a5a5c3afdbea8cc18f6eb7f3a5
tom <tom@that-place>
2019-10-31 18:28:14 -0500

    Single ns, removed deps

--- project.clj
+++ project.clj
@@ -4,7 +4,5 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [fn-fx/fn-fx-javafx "0.5.0-SNAPSHOT"]
-                 [me.raynes/fs "1.4.6"]
                  [cheshire "5.9.0"]]
   :repl-options {})
--- src/vegafx/core.clj
+++ src/vegafx/core.clj
@@ -1,109 +1,236 @@
 (ns vegafx.core
-  (:require
-   [fn-fx.fx-dom :as dom]
-   [fn-fx.controls :as ui]
-   [fn-fx.diff :refer [component defui render should-update?]]
-   [fn-fx.util :refer [run-later]]
-   [vegafx.template :as template]))
+  (:import [javafx.application Platform]
+           [javafx.beans.value ChangeListener]
+           [javafx.concurrent Worker$State]
+           [javafx.embed.swing JFXPanel]
+           [javafx.scene Scene]
+           [javafx.scene.web WebView WebEvent WebEngine]
+           [javafx.stage Stage]
+           [javafx.embed.swing SwingFXUtils]
+           [javax.imageio ImageIO])
+  (:require [clojure.java.io :as io]
+            [cheshire.core :as cheshire]))
 
-(def initial-state {:current-url nil})
+;;quickie translated  from https://stackoverflow.com/a/23979996
+(defn b64->image-bytes ^bytes [data]
+  (let [[type image]  (clojure.string/split  data #",")
+        ^bytes image-bytes (javax.xml.bind.DatatypeConverter/parseBase64Binary
+                            ^String image)]
+    {:type type
+     :buffer image-bytes}))
 
-(defonce app-state (atom initial-state))
-(defonce app-ui-state (agent nil))
+(defn spit-image-bytes [{:keys [type buffer]} tgt]
+  (let [ext (case type
+              "data:image/jpeg;base64" "jpeg"
+              "data:image/png;base64"  "png"
+              "jpg")
+        path (str tgt "." ext)]
+    (with-open [out (io/output-stream (io/file path))]
+      (.write out ^bytes buffer))))
 
-(defmulti handle-event (fn [state {:keys [event]}] event))
+(defn spit-image [data tgt]
+  (-> data b64->image-bytes (spit-image-bytes tgt)))
 
-(defn get-engine! []
-  (.getEngine (.lookup (.getScene @(:root @app-ui-state)) "#web-browser")))
+(defonce force-toolkit-init (javafx.embed.swing.JFXPanel.))
 
-(defn load! [url]
-  (fn-fx.util/run-and-wait
-   (.load (get-engine!) url)))
+(defn run-later*
+  [f]
+  (javafx.application.Platform/runLater f))
 
-;; engine.getLoadWorker().stateProperty().addListener((obs, oldState, newState) ->
-;;{if (newState == Worker.State.SUCCEEDED)
-;;    {// new page has loaded, process:
-;;     testMethod();}})
+(defmacro run-later
+  [& body]
+  `(run-later* (fn [] ~@body)))
 
-(defn change-fn [f]
-  (reify javafx.beans.value.ChangeListener
-    (changed [this obs oldv newv]
-      (f obs oldv newv))))
-
-(defn watch-load! [f]
-  (fn-fx.util/run-and-wait
-   (-> (get-engine!)
-       (.getLoadWorker)
-       (.stateProperty)
-       (.addListener
-        (change-fn (fn [obs old-state new-state]
-                     (when (= new-state javafx.concurrent.Worker$State/SUCCEEDED) 
-                       (f obs old-state new-state))))))))
-
-(defn load-url [url]
-  (let [engine (get-engine!)]
+(defn run-now*
+  [f]
+  (let [result (promise)]
     (run-later
-     (.load engine url))))
+     (deliver result (try (f) (catch Throwable e e))))
+    @result))
 
-(defmethod handle-event :load-url
-  [state {:keys [fn-fx/includes]}]
-  (let [new-url (get-in includes [:url-field :text])]
-    (load-url new-url)
-    (assoc-in state [:current-url] new-url)))
+(defmacro run-now
+  [& body]
+  `(run-now* (fn [] ~@body)))
 
-#_(defui WebBrowser
-  (render [this args]
-          (ui/v-box
-           ;;:style "-fx-base: rgb(30, 30, 35);"
-           :padding (ui/insets
-                     :top-right-bottom-left 25)
-           :children [(ui/text-field
-                       :id :url-field
-                       :prompt-text "Url: "
-                       :font (ui/font :family "Helvetica" :size 20)
-                       :on-action {:event :load-url
-                                   :fn-fx/include {:url-field #{:text}}})
-                      (ui/web-view :id "web-browser")])))
+(def ^:dynamic *buffer* 20)
+(defonce ^:dynamic *web-view* (atom nil))
 
-(defui WebBrowser
-  (render [this args]
-    (ui/web-view :id "web-browser")))
+(defn eval-js [^String script]
+  (let [e (.getEngine ^WebView  @*web-view*)]
+    (run-now
+     (.executeScript ^WebEngine e script))))
 
-(defui MainStage
-  (render [this args]
-          (ui/stage :title "Web Browser Main Stage"
-                    :min-width (get args :min-width 1024)
-                    :min-height (get args :min-height 768)
-                    :shown true
-                    :scene (ui/scene :root (web-browser args)))))
+#_(defn vega-script
+  [edn]
+  (str "vegaEmbed(\"#vis\","
+       (cheshire/generate-string edn)
+       ",{\"actions\":false})"))
 
-;;some of this is extraneous, likely chopped since we're not using
-;;a browser.
-(defn ui-event-handler [event]
-  (try
-    (swap! app-state handle-event event)
-    (catch Throwable ex
-      (println (str "Error updating app data state! " ex)))))
+(def embed (slurp (clojure.java.io/resource "vegaembed.js")))
 
-(defn update-ui-state-agent [old-ui]
-  (try
-    (dom/update-app old-ui (main-stage @app-state))
-  (catch Throwable ex
-    (println (str "Error updating app UI state! " ex)))))
+(defn vega-script [spec]
+  (let [spec (if (string? spec)
+               spec
+               (cheshire/generate-string spec))]
+  (clojure.string/replace embed "var spec = \"THE-SPEC\""
+     (str "var spec = " spec))))
 
-(defn run-webview-app
-  ([args]
-   (let [u (main-stage args)
-         ui-state (dom/app (main-stage @app-state) ui-event-handler)]
-     (send app-ui-state (fn [old-state]  ui-state))
-     (add-watch app-state :ui (fn [key atom old-state new-state]
-                                (send app-ui-state #'update-ui-state-agent)))
-     (watch-load! (fn [& args] (println :loaded-page!)))))
-  ([] (run-webview-app {})))
+(defn static-script [script]
+  (clojure.string/replace script "noisy = true" "noisy = false"))
 
-(defn view-chart [spec & {:keys [min-width min-height] :or {min-width 1024 min-height 768} :as args}]
-  (run-webview-app args)
-  (load! (str (clojure.java.io/as-url (template/spit-chart spec)))))
+(defn chart-html [edn]
+  (clojure.string/replace
+   (slurp (io/resource "index.html"))
+   "</body>"
+   (clojure.string/join \newline ["</body>"
+                                  "<script>"
+                                  (static-script (vega-script edn))
+                                  "</script>"])))
 
-(defn -main [& args]
-  (run-webview-app))
+;; engine.onAlert = new EventHandler<WebEvent<String>>()
+;; {@Override
+;;  void handle(WebEvent<String> event)
+;;  {if("command:ready".equals(event.getData()))
+;;   {//TODO: initialize
+;;    }
+;;   }
+;;  }
+
+;;we wait for an alert from the browser.
+(defn ^javafx.event.EventHandler on-ready [f]
+  (reify javafx.event.EventHandler
+    (handle [this event]
+      (f (.getData ^WebEvent event)))))
+
+(def alerts
+  {"data:image" :image-ready
+   "chart-ready" :ready
+   "image-ready" :image-ready})
+
+(defn alert-dispatch [msg]
+  (reduce-kv (fn [acc k v]
+               (if (clojure.string/includes? msg k)
+                 (reduced v)
+                 acc)) nil alerts))
+
+(defmulti alert-handler alert-dispatch)
+(defmethod alert-handler :default [msg]
+  {:alert :generic
+   :data  msg})
+
+(defmethod alert-handler :ready [msg]
+  {:alert :ready
+   :data  msg})
+
+(defmethod alert-handler :image-ready [msg]
+  {:alert :image-ready
+   :data msg})
+
+(defn dimensions
+  [engine id]
+  {:height (.executeScript engine "document.getElementById('vis').offsetHeight")
+   :width (.executeScript engine "document.getElementById('vis').offsetWidth")})
+
+(defn vega-lite
+  [edn & {:keys [show?]}]
+  (run-now
+   (let [web-view (WebView.)
+         engine (.getEngine web-view)
+         ready  (promise)
+         image-url (promise)
+         show   (fn [^Stage x] (if show? (doto x (.show)) x))]
+     (Platform/setImplicitExit false)
+     (.. engine getLoadWorker stateProperty
+         (addListener
+          (reify ChangeListener
+            (changed [this ob old new]
+              ;; Wait until the page has loaded.
+              (when (= new Worker$State/SUCCEEDED)
+                (.executeScript engine (vega-script edn))
+                (let [{:keys [height width]} (dimensions engine "vis")]
+                  (.setPrefSize web-view
+                                (+ *buffer* width)
+                                (+ *buffer* height))
+                  (doto (Stage.)
+                    (.setScene (Scene. web-view))
+                    (show))))))))
+     (.setOnAlert engine ^javafx.event.EventHandler
+                  (on-ready (fn [msg]
+                              (let [k (alert-handler msg)]
+                                (case (:alert k)
+                                  :ready       (deliver ready msg)
+                                  :image-ready (deliver image-url msg)
+                                  :generic     (deliver image-url msg))))))
+     (reset! *web-view* web-view)
+     (.loadContent engine (slurp (io/resource "index.html")))
+     {:ready-promise ready
+      :image-url     image-url})))
+
+;;may still be useful?
+(defn take-snapshot
+  []
+  (run-later
+   (try
+     (let [web-view @*web-view*
+           jfx-img (.snapshot web-view
+                              (javafx.scene.SnapshotParameters.)
+                              nil)
+           buf-img (SwingFXUtils/fromFXImage jfx-img nil)]
+       (ImageIO/write buf-img "PNG" (java.io.File. "test.png")))
+     (catch Throwable e
+       (println e)))))
+
+(defn capture-image [& {:keys [format] :or {format :png}}]
+  (assert #{:png :svg "png" "svg"} format)
+  (eval-js (str "getImage('" (name format) "')")))
+
+#_(defn render-and-save [spec & {:keys [show?]}]
+    (let [plot (vega-lite spec :show? show?)
+          _    @(:ready-promise plot)]
+      (take-snapshot)))
+
+(defn vega->image [spec tgt & {:keys [show? format] :or {format :png}}]
+  (let [{:keys [ready-promise image-url ]} (vega-lite spec :show? show?)
+        _    @ready-promise
+        _   (capture-image  :format format)]
+    (case format
+      :svg (spit (str tgt ".svg") @image-url)
+      (spit-image @image-url tgt))))
+
+(defn vega->html [spec tgt]
+  (spit (str tgt ".html") (chart-html spec)))
+
+;;temp
+(defn test-batch []
+  (let [colors ["red" "white" "blue"]]
+    (doseq [c colors]
+      (let [spec (assoc test-spec "background" c)
+            tgt  (str "./examples/" c)]
+        (println [:spitting c])
+        (vega->image spec tgt)
+        (vega->image spec tgt :format :svg)
+        (vega->html  spec tgt)))))
+
+(comment
+
+  (set! *warn-on-reflection* true)
+  (def test-spec {"$schema" "https://vega.github.io/schema/vega-lite/v2.0.json"
+                  "data" {"values" [{"a" "A","b" 100}
+                                    {"a" "B","b" 55}
+                                    {"a" "C","b" 43}
+                                    {"a" "D","b" 91}
+                                    {"a" "E","b" 81}
+                                    {"a" "F","b" 53}
+                                    {"a" "G","b" 19}
+                                    {"a" "H","b" 87}
+                                    {"a" "I","b" 52}]}
+                  "mark" "bar"
+                  "encoding" {"x" {"field" "a", "type" "ordinal"}
+                              "y" {"field" "b", "type" "quantitative"}}
+                  "background" "white"})
+
+  ;(vega-lite test-spec)
+  ;;  (render-and-save test-spec)
+  (def res (vega-lite test-spec))
+  (spit-image (-> res :image-url deref) "blah")
+  )

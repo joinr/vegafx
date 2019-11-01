425bcfb10561e9810a8008469d363fcb8ac69cc3
tom <tom@that-place>
2019-10-31 21:45:17 -0500

    Added openjfx profile

--- project.clj
+++ project.clj
@@ -5,4 +5,13 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [cheshire "5.9.0"]]
-  :repl-options {})
+  :profiles {;;cribbed from fn-fx
+             :openjfx11      ^:leaky   ; Ensure these dependencies "leak" through to the POM and JAR tasks
+             {:dependencies [[org.openjfx/javafx-controls "11.0.2"]
+                             [org.openjfx/javafx-swing    "11.0.2"]
+                             [org.openjfx/javafx-media    "11.0.2"]
+                             [org.openjfx/javafx-fxml     "11.0.2"]
+                             [org.openjfx/javafx-web      "11.0.2"]
+                             [org.openjfx/javafx-graphics "11.0.2"]]}}
+  :repl-options {}
+  )
